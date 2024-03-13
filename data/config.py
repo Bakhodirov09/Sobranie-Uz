fd277cd77ecd9819142b9939aa521b7da60354c1
Bahodirov Nurulloh <bloghash01@gmail.com>
2024-03-13 06:19:08 +0500

    alembic versions del

--- data/config.py
+++ data/config.py
@@ -1,10 +1,8 @@
 from environs import Env
 
-# environs kutubxonasidan foydalanish
 env = Env()
 env.read_env()
 
-# .env fayl ichidan quyidagilarni o'qiymiz
-BOT_TOKEN = env.str("BOT_TOKEN")  # Bot toekn
+BOT_TOKEN = env.str("BOT_TOKEN")
 ADMINS = env.list("ADMINS")
 DATABASE_PRO = env.str("DATABASE_PRO")
